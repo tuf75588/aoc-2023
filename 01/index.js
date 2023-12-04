0f9caba6e9c80d362c4c67894f69b1d6a69a5f3c
Andrew Davis <atd285@gmail.com>
2023-12-04 15:53:01 -0500

    day one part two

--- 01/index.js
+++ 01/index.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function partOne(file) {
   const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
@@ -7,28 +7,83 @@ function partOne(file) {
     let first = line.split('').find((v) => !Number.isNaN(Number(v)));
     let last = line.split('').findLast((b) => !Number.isNaN(Number(b)));
     return Number(first + last);
-  })
-  return values.reduce((a,c) => {
+  });
+  return values.reduce((a, c) => {
     return a + c;
   });
 }
 
-console.log(partOne('./input.txt')); // 55017
+// console.log(partOne('./input.txt')); // 55017
 /* PART 2 */
 
 let legend = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
 };
 
+const firstNumberWordsRegExp = new RegExp(
+  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].join(
+    '|'
+  )
+);
+
+const lastNumberWordsRegExp = new RegExp(
+  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    .join('|')
+    .split('')
+    .reverse()
+    .join('')
+);
 
 function partTwo(file) {
-  console.log(file);
+  const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
+  const values = lines.map((line) => {
+    // first
+    let firstNumberIndex = line
+      .split('')
+      .findIndex((c) => !Number.isNaN(Number(c)));
+    let firstWordMatch = line.match(firstNumberWordsRegExp);
+    let firstWordNumberIndex = firstWordMatch?.index;
+
+    let firstNumber =
+      firstNumberIndex !== -1
+        ? firstWordMatch
+          ? firstNumberIndex < firstWordNumberIndex
+            ? line[firstNumberIndex]
+            : legend[firstWordMatch[0]]
+          : line[firstNumberIndex]
+        : legend[firstWordMatch[0]];
+
+    // last
+    let lastNumberIndex = line
+      .split('')
+      .findLastIndex((c) => !Number.isNaN(Number(c)));
+    let lastWordMatch = line
+      .split('')
+      .reverse()
+      .join('')
+      .match(lastNumberWordsRegExp);
+    let lastWordNumberIndex = lastWordMatch ? line.length - 1 - lastWordMatch.index : null;
+    let lastNumber =
+      lastNumberIndex !== -1
+        ? lastWordMatch 
+          ? lastNumberIndex > lastWordNumberIndex
+            ? line[lastNumberIndex]
+            : legend[lastWordMatch[0].split('').reverse().join('')]
+          : line[lastNumberIndex]
+        : legend[lastWordMatch[0].split('').reverse().join('')];
+
+    return Number(firstNumber + lastNumber);
+  });
+  return values.reduce((s, v) => s + v);
 }
+
+console.log(partTwo('./input.txt'));
+console.log(partTwo('./example-input.txt'));
