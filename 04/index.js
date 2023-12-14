cf5ea78bd90b99e1ddfe15a7c366737d5e8fea9d
Andrew Davis <atd285@gmail.com>
2023-12-14 16:02:30 -0500

    day 4 part one

--- 04/index.js
+++ 04/index.js
@@ -1,16 +1,67 @@
 import fs from 'fs';
 
 function formatInput(file) {
-   return fs.readFileSync(file, 'utf-8').trim().split('\n');
+  return fs.readFileSync(file, 'utf-8').trim().split('\n');
 }
 
+function format(lines) {
+  const games = formatInput(lines).map((line, lineIdx) => {
+    const [cardNumber, numbers] = line.split(': ');
 
-function compareArray(lines) {
-  const games = formatInput(lines).map((item, index) => {
-      const card = item.split(' | ');
-      console.log({card});
-  })
+    const [winnersStr, yoursStr] = numbers.split(' | ');
+
+    // formatting
+    return {
+      id: cardNumber.replace(/Card\s+/, ''),
+      winners: new Set(winnersStr.split(' ').filter(Boolean).map(Number)),
+      yours: new Set(yoursStr.split(' ').filter(Boolean).map(Number)),
+    };
+  });
   return games;
 }
 
-console.log(compareArray('./example-input.txt'));
+function intersection(...sets) {
+  return sets.reduce((set1, set2) => {
+    const results = new Set();
+
+    for (const item of set2) {
+      if (set1.has(item)) {
+        results.add(item);
+      }
+    }
+    return results;
+  });
+}
+
+function findMatches({ winners, yours }) {
+  return intersection(winners, yours);
+}
+
+function getWorth(set) {
+  return set.size ? 2 ** (set.size - 1) : 0;
+}
+
+
+function solution1() {
+  const scratchcards = format('./input.txt');
+  const vals = scratchcards.map(findMatches).map(getWorth);
+  return vals.reduce((a,b) => a  + b);
+}
+
+
+console.log(solution1());
+
+function part2() {
+  const scrarchcards = format('./example-input.txt');
+  const copyQuantitiesById = {};
+
+  function processCard(card, cardIdx) {
+    const matches = findMatches(card);
+    if (!matches.size) return;
+    let i = 0;
+    for (i; i < matches.size; i++) {
+      let nextIdx= cardIdx + 1 + i;
+    }
+  }
+
+}
