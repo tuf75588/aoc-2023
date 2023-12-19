99420a78d6f5cdfe84e266e3eacab7ccda2a7544
Andrew Davis <atd285@gmail.com>
2023-12-19 13:26:06 -0500

    I HATE DAY 5

--- 05/index.js
+++ 05/index.js
@@ -1,13 +1,151 @@
-import { readFileSync } from 'fs';
+const fs = require('fs')
+const path = require('path')
 
-const data = readFileSync('./example-input.txt', 'utf-8');
-
-function partOne(input) {
-  let [seeds, ...maps] = input.trim().split('\n\n');
-  return {
-    seeds: seeds.split(': ').at(1).split(' ').map(Number),
-    // maps: maps.map(Number),
-  };
+function getInput(dir) {
+  return fs.readFileSync(path.resolve(dir, './input.txt'), {
+    encoding: 'utf-8',
+  })
 }
 
-console.log(partOne(data));
+const data = getInput(__dirname)
+
+const formatInputPart1 = input => {
+  const [seeds, ...maps] = input.trim().split('\n\n')
+
+  return {
+    seeds: seeds.split(': ').at(1).split(' ').map(Number),
+    maps: maps.map(parseMap),
+  }
+}
+
+class Range {
+  constructor(start, length) {
+    this.start = start
+    this.end = start + length - 1
+  }
+
+  has(value) {
+    return value >= this.start && value <= this.end
+  }
+}
+
+class RangeMap {
+  constructor(dest, src, length) {
+    this.srcRange = new Range(src, length)
+    this.destRange = new Range(dest, length)
+    this.offset = dest - src
+  }
+
+  getValue(key) {
+    return this.srcRange.has(key) ? key + this.offset : null
+  }
+}
+
+class RangeGroup {
+  constructor(rangeMaps) {
+    this.rangeMaps = rangeMaps
+  }
+
+  getValue(key) {
+    for (const map of this.rangeMaps) {
+      const nextKey = map.getValue(key)
+
+      if (!nextKey) continue
+
+      return nextKey
+    }
+
+    return key
+  }
+}
+
+const parseMap = section => {
+  const ranges = section
+    .split('map:')
+    .at(1)
+    .trim()
+    .split('\n')
+    .map(line => line.split(' ').map(Number))
+  const rangeMaps = ranges
+    .map(range => new RangeMap(...range))
+    .sort((a, b) => a.start - b.start)
+
+  return new RangeGroup(rangeMaps)
+}
+
+function solution1(input) {
+  const { seeds, maps } = formatInputPart1(input)
+
+  const locations = seeds.map(seed => {
+    let result = seed
+
+    for (const map of maps) {
+      result = map.getValue(result)
+    }
+
+    return result
+  })
+
+  return Math.min(...locations)
+}
+
+function makeSeedRanges(seeds) {
+  const nums = seeds.split(': ')[1].split(' ').map(Number)
+  const result = []
+
+  while (nums.length) {
+    const start = nums.shift()
+    const length = nums.shift()
+
+    result.push(new Range(start, length))
+  }
+
+  return result
+}
+
+function parseMapsReverse(section) {
+  const ranges = section
+    .split('map:')
+    .at(1)
+    .trim()
+    .split('\n')
+    .map(line => line.split(' ').map(Number))
+
+  // We're reversing the maps in part 2
+  const rangeMaps = ranges
+    .map(([dest, src, length]) => new RangeMap(src, dest, length))
+    .sort((a, b) => a.start - b.start)
+
+  return new RangeGroup(rangeMaps)
+}
+
+function formatInputPart2(input) {
+  const [seeds, ...maps] = input.trim().split('\n\n')
+
+  return {
+    seeds: makeSeedRanges(seeds),
+    maps: maps.reverse().map(parseMapsReverse),
+  }
+}
+
+function solution2(input) {
+  const { seeds, maps } = formatInputPart2(input)
+
+  let i = 0
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    let result = i
+    for (const map of maps) {
+      result = map.getValue(result)
+    }
+
+    for (const seedRange of seeds) {
+      if (seedRange.has(result)) return i
+    }
+
+    i++
+  }
+}
+
+// console.log(solution1(data))
+console.log(solution2(data))
