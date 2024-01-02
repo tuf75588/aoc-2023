b7fd2e0383893f05241707986f5ae081237d2406
Andrew Davis <atd285@gmail.com>
2024-01-02 16:26:41 -0500

    day 6 work

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,19 @@
 const fs = require('fs');
 
+const data = fs.readFileSync('./example-input.txt', 'utf-8');
+
+function getTimesAndDistances(timesLine, distancesLine) {
+  const times = timesLine
+    .replace(/Time:\s+/, '')
+    .split(' ')
+    .filter(Boolean)
+    .map(Number);
+
+  const distances = distancesLine
+    .replace(/Distance:\s+/, '')
+    .split(' ')
+    .filter(Boolean)
+    .map(Number);
+
+  return { times, distances };
+}
