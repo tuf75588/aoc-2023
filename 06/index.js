92cf571ff3c068a039003534dbf602ded9e13d1d
Andrew Davis <atd285@gmail.com>
2024-01-02 16:38:24 -0500

    day 6 part 1

--- 06/index.js
+++ 06/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('./example-input.txt', 'utf-8');
+const data = fs.readFileSync('./input.txt', 'utf-8');
 
 function getTimesAndDistances(timesLine, distancesLine) {
   const times = timesLine
@@ -17,3 +17,40 @@ function getTimesAndDistances(timesLine, distancesLine) {
 
   return { times, distances };
 }
+
+function getRaces(times, distances) {
+  const races = [];
+  while (times.length && distances.length) {
+    const time = times.shift();
+    const distance = distances.shift();
+    races.push({ time, distance });
+  }
+  return races;
+}
+
+function getRaceResults(race) {
+  const results = [];
+
+  for (let i = 0; i <= race.time; i++) {
+    const speed = i * 1;
+    const time = race.time - i;
+    const distance = speed * time;
+    results.push({ distance });
+  }
+  return results;
+}
+
+function solution1(input) {
+  const [timesLine, distancesLine] = data.trim().split('\n');
+  const { times, distances } = getTimesAndDistances(timesLine, distancesLine);
+  const races = getRaces(times, distances);
+
+  const wins = races.map(
+    (race) =>
+      getRaceResults(race).filter((result) => result.distance > race.distance)
+        .length
+  );
+  return wins.reduce((a,b) => a * b);
+}
+
+console.log(solution1(data));
